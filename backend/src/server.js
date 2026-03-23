c05e9aeae3c35446b6ae8fe54d898f7bb73bbb85
TONYSTARK363 <clgproject662@gmail.com>
2026-03-23 01:06:30 -0400

    update : netlfy link update

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,9 @@ const app = express();
 //Cors Setup
 app.use(
     cors({
-   origin:'http://127.0.0.1:5500',
+   origin:['http://127.0.0.1:5500',
+    'https://clgattendance1.netlify.app'],
+
    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
      allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
